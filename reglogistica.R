bc97363beb3c37f9453dc7bf6b0daa954c81d750
Carlos <caarroyavepadilla@gmail.com>
2019-03-31 23:21:31 -0600

    reglog con arboles and shit

--- reglogistica.R
+++ reglogistica.R
@@ -2,6 +2,8 @@ library(caret)
 library(corrplot)
 library(dummy)
 library(plyr)
+library(randomForest)
+library(e1071)
 
 setwd("D:/mineria/hoja3/Mineria-Arboles/")
 entrenamiento = read.csv("train.csv") 
@@ -10,8 +12,8 @@ entrenamiento1 <- data.frame(entrenamiento)
 prueba1 <- data.frame(prueba)
 total <- merge(entrenamiento1, prueba1, all = TRUE)
 entrenamiento1$AdoptionSpeed <- as.factor(entrenamiento1$AdoptionSpeed)
-entrenamiento1$Velocidad <- revalue(entrenamiento1$AdoptionSpeed, c("0" = "1", "1"= "1", "2"="1", "3"="0","4"="0")) 
 
+entrenamiento1$Velocidad <- revalue(entrenamiento1$AdoptionSpeed, c("0" = "1", "1"= "1", "2"="1", "3"="0","4"="0")) 
 
 
 porciento <- 0.7
@@ -21,11 +23,32 @@ train <- entrenamiento1[trainRowsNumber, ]
 train<- train[complete.cases(train),]
 test <- entrenamiento1[-trainRowsNumber, ]
 test <- test[complete.cases(test),]
-train1 <- within(train, rm("Name", "PetID", "Description", "RescuerID"))
-modelo <- glm(train$Velocidad~Age+Breed1, data = train1, family = binomial())
+train1 <- within(train, rm("Name", "PetID", "Description", "RescuerID", "AdoptionSpeed"))
+
+t <- sapply(train1,as.numeric)
+correl1 <- cor(t, method = "spearman")
+corrplot(correl1)
+
+#Regresion Logistica
+modelo <- glm(train1$Velocidad~., data = train1, family = binomial())
 modelo
+plot(modelo)
 prediccion <- predict(modelo,newdata = test, type="response")
-as.factor(prediccion)
-prediccion<-ifelse(prediccion>=0.5,1,0)
+prediccion<-ifelse(prediccion>=0.5,0,1)
 cfm <- confusionMatrix(as.factor(test$Velocidad),as.factor(prediccion))
 cfm
+
+#arbol
+
+modeloRF1<-randomForest(train1$Velocidad~.,data=train1)
+prediccionRF1<-predict(modeloRF1,newdata = test)
+cfmRandomForest <- confusionMatrix(as.factor(test$Velocidad), as.factor(prediccionRF1))
+cfmRandomForest
+
+#Bayes
+modeloBayes<-naiveBayes(train1$Velocidad~.,data=train1)
+predBayes<-predict(modeloBayes, newdata = test)
+cfm<-confusionMatrix(as.factor(test$Velocidad), as.factor(predBayes))
+cfm
+modelo
+
